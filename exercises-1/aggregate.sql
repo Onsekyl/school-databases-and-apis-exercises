b652b7d3f43196f9be0a488a3e2c81ee686af99d
Kimi Leinola <154047539+Onsekyl@users.noreply.github.com>
2026-01-13 12:16:04 +0200

    Half way of aggregate exercise

--- exercises-1/aggregate.sql
+++ exercises-1/aggregate.sql
@@ -1,5 +1,14 @@
 USE project;
-SELECT  * from project;
+SELECT * FROM project;
+Select * FROM person;
+Select * FROM hour;
 
 SELECT MAX(salary), MIN(salary) FROM person;
-SELECT COUNT(*) FROM project;
+SELECT COUNT(*) AS 'Rows in project' FROM project;
+SELECT AVG(birth_year) FROM person;
+SELECT COUNT(city) AS 'Amount of persons from Turku' FROM person WHERE city LIKE 'Turku';
+SELECT SUM(salary) AS 'Summed salaries of people from Turku' FROM person WHERE city LIKE 'Turku';
+SELECT city, SUM(salary) as 'Sum of salaries' FROM person GROUP BY city;
+
+# Find a way to always format to 0.00!
+SELECT city, ROUND(AVG(salary), 2) as 'Average salaries' FROM person GROUP BY city;
